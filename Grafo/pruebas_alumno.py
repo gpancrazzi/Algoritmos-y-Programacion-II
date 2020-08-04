7d9b66335e1b79c930d6a7394f1b4beb822421d1
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-04 12:27:30 -0300

    Se agregan algunas pruebas unitarias

--- Grafo/pruebas_alumno.py
+++ Grafo/pruebas_alumno.py
@@ -14,7 +14,10 @@ def prueba_grafo_dirigido():
         grafo1.agregar_vertice(equipo, None)
     if not grafo1.vertices_estan_unidos(equipos[0], equipos[2]): print('Prueba grafo no hay arista entre dos vertices... Ok')
     grafo1.agregar_arista(equipos[0], equipos[2], None)
-    if grafo1.vertices_estan_unidos(equipos[0], equipos[2]): print('Prueba grafo se agrega una arista... Ok')
+    if grafo1.vertices_estan_unidos(equipos[0], equipos[2]) and grafo1.vertices_estan_unidos(equipos[2], equipos[0]): 
+        print('Prueba grafo se agrega una arista... Ok')
+    if grafo1.cantidad_aristas == 1: print('Prueba grafo la cantidad de aristas es la correcta... Ok')
+    if grafo1.cantidad_vertices() == 5: print('Prueba grafo la cantidad de vertices es la correcta... Ok')
 
 prueba_grafo_crear()
 prueba_grafo_dirigido()
