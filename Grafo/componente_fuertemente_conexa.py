420f84c24fa42c3e3cd0adbb0b4e323de51de809
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-07 16:32:25 -0300

    Se agrega modulo de componente fuertemente conexa

---
+++ Grafo/componente_fuertemente_conexa.py
@@ -0,0 +1,30 @@
+from grafo import Grafo
+from pila import Pila
+
+def minimo(orden1, orden2):
+    """"""
+    if orden1 <= orden2:
+        return orden1
+    return orden2
+
+def componente_fuertemente_conexa(grafo, vertice, visitados, pila, apilados, orden, mas_bajo, componentes):
+    """"""
+    visitados.add(vertice)
+    pila.apilar(vertice)
+    apilados.add(vertice)
+    mas_bajo[vertice] = orden[vertice]
+    for adyacente in grafo.obtener_adyacentes(vertice):
+        if adyacente not in visitados:
+            orden[adyacente] = orden[vertice] + 1
+            componente_fuertemente_conexa(grafo, adyacente, visitados, pila, apilados, orden, mas_bajo, componentes)
+            mas_bajo[vertice] = minimo(mas_bajo[vertice], mas_bajo[adyacente])
+        else if adyacente in apilados:
+            mas_bajo[vertice] = minimo(mas_bajo[vertice], mas_bajo[adyacente])
+    if mas_bajo[vertice] == orden[vertice]:
+        nueva_cfc = []
+        componente = pila.desapilar()
+        while componente != vertice:
+            apilados.remove()
+            nueva_cfc.append(componente)
+            componente = pila.desapilar()
+        componentes.append(nueva_cfc)
