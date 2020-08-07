54cc204a25968fd53cdb0983f9a155c67d0fad3c
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-07 16:31:41 -0300

    Se crea TDA pila

---
+++ Grafo/pila.py
@@ -0,0 +1,24 @@
+class Pila:
+     def __init__(self):
+        """"""
+        self.items = []
+
+     def esta_vacia(self):
+        """"""
+        return self.items == []
+
+     def apilar(self, item):
+        """"""
+        self.items.append(item)
+
+     def desapilar(self):
+        """"""
+        return self.items.pop()
+
+     def ver_tope(self):
+        """"""
+        return self.items[len(self.items)-1]
+
+     def tamano(self):
+        """"""
+        return len(self.items)
