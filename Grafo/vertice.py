e4d0b8f1e5b7852f3db16c383694d37161082914
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-09 22:55:30 -0300

    cambios menores

--- Grafo/vertice.py
+++ Grafo/vertice.py
@@ -23,10 +23,10 @@ class Vertice(object):
         Muestra los adyacentes del vertice"""
         return self.vecinos.keys()
     
-    def agregar_union(self, vertice, dato=None):
+    def agregar_union(self, vertice, peso=None):
         """Pre: el vertice existe.
         Agrega un adyacente al vertice"""
-        self.vecinos[vertice] = dato
+        self.vecinos[vertice] = peso
 
     def eliminar_union(self, vertice):
         """Pre: el vertice existe y la arista existen.
@@ -35,24 +35,20 @@ class Vertice(object):
 
     def ver_peso_union(self, vertice):
         """Pre: el vertice y la arista existen.
-        Muestra el dato de la unión de los vertices.
-        """
+        Muestra el dato de la unión de los vertices."""
         return self.vecinos.get(vertice)
 
     def cantidad_uniones(self):
         """Pre: el vertice existe.
-        Se muestran la cantidad de adyacentes que tiene el vertice.
-        """
+        Se muestran la cantidad de adyacentes que tiene el vertice."""
         return len(self.vecinos)
 
     def estan_conectados(self, vertice):
         """Pre: los vertices existen.
-        Retorna true si existe una arista entre ambos vertices y false en caso contrario.
-        """
+        Retorna true si existe una arista entre ambos vertices y false en caso contrario."""
         return vertice in self.vecinos
 
     def iterador_vertice(self):
         """Pre: el vertice existe.
-        Retorna un iterador para recorrer los adyacentes al vertice.
-        """
+        Retorna un iterador para recorrer los adyacentes al vertice."""
         return iter(self.vecinos)
