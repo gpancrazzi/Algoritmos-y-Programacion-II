740e8ee6cf72515049ff01de674c61b79192f22f
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-09 22:56:42 -0300

    Se agregan pruebas

--- Grafo/pruebas_alumno.py
+++ Grafo/pruebas_alumno.py
@@ -13,7 +13,7 @@ def prueba_grafo_dirigido():
     equipos = ['River', 'Boca', 'Independiente', 'San Lorenzo', 'Racing']
     for equipo in equipos:
         grafo1.agregar_vertice(equipo, None)
-    print_test('Prueba grafo no hay arista entre dos vertices', not grafo1.vertices_estan_unidos(equipos[0], equipos[2]))
+    print_test('Prueba grafo dirigido no hay arista entre dos vertices', not grafo1.vertices_estan_unidos(equipos[0], equipos[2]))
     for i in [2,3,4]:
         grafo1.agregar_arista(equipos[0], equipos[i], None)
     for i in [0,2,4]:
@@ -22,15 +22,36 @@ def prueba_grafo_dirigido():
         grafo1.agregar_arista(equipos[2], equipos[i], None)
     grafo1.agregar_arista(equipos[3], equipos[1], None)
     grafo1.agregar_arista(equipos[4], equipos[3], None)
-    print_test('Prueba grafo se agregan aristas y la cantidad de aristas es la correcta', grafo1.cantidad_aristas == 10)
-    print_test('Prueba grafo la cantidad de vertices es la correcta', grafo1.cantidad_vertices() == 5)
+    print_test('Prueba grafo dirigido se agregan aristas y la cantidad de aristas es la correcta', grafo1.cantidad_aristas == 10)
+    print_test('Prueba grafo dirigido la cantidad de vertices es la correcta', grafo1.cantidad_vertices() == 5)
     hijos_river = grafo1.obtener_adyacentes(equipos[0])
     river_padre = {'Independiente', 'San Lorenzo', 'Racing'}
     ok = True
     for hijo in hijos_river:
         ok = hijo in river_padre
-    print_test('Prueba grafo obtener adyacentes a un vertice retorna los valores correctos', ok)
+    print_test('Prueba grafo dirigido obtener adyacentes a un vertice retorna los valores correctos', ok)
+    grafo1.eliminar_vertice(equipos[4])
+    print_test('Prueba grafo dirigido se elimina un vertice', not grafo1.pertenece_vertice(equipos[4]))
+    print_test('Prueba grafo dirigido se elimina un vertice y la cantidad de aristas es la correcta', grafo1.cantidad_uniones() == 6)
+    print_test('Prueba grafo dirigido arista ya no existe', not grafo1.vertices_estan_unidos(equipos[4], equipos[3]))
+    print_test('Prueba grafo dirigido arista ya no existe', not grafo1.vertices_estan_unidos(equipos[0], equipos[4]))
+
+def prueba_grafo_no_dirigido():
+    grafo1 = Grafo(False)
+    paises = ['Argentina', 'Brasil', 'Chile', 'Bolivia', 'Paraguay', 'Uruguay']
+    for pais in paises:
+        grafo1.agregar_vertice(pais, None)
+    for i in [1,2,3,4,5]:
+        grafo1.agregar_arista(paises[0], paises[i])
+    print_test('Prueba grafo no dirigido', not grafo1.es_dirigido)
+    print_test('Prueba grafo no dirigido la cantidad de vertices es la correcta', grafo1.cantidad_vertices() == 6)
+    print_test('Prueba grafo no dirigido la cantidad de aristas es la correcta', grafo1.cantidad_uniones() == 5)
+    grafo1.eliminar_vertice(paises[0])
+    print_test('Prueba grafo no dirigido se elimina un vertice', not grafo1.pertenece_vertice(paises[0]))
+    print_test('Prueba grafo no dirigido se elimina un vertice y la cantidad de aristas es la correcta', grafo1.cantidad_uniones() == 0)
+    print_test('Prueba grafo no dirigido arista ya no existe', not grafo1.vertices_estan_unidos(paises[0], paises[3]))
 
 print("~~~ PRUEBAS ALUMNO ~~~")
 prueba_grafo_crear()
 prueba_grafo_dirigido()
+prueba_grafo_no_dirigido()
