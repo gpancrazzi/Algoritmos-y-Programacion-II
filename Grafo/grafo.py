653b82ad27a7df5db0a76e1eea8d60d009a37ad1
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-09 22:54:16 -0300

    Se corrige el TDA grafo y se agregan primitivas

--- Grafo/grafo.py
+++ Grafo/grafo.py
@@ -16,25 +16,8 @@ class Grafo(object):
         if nombre in self.vertices: return False
         self.vertices[nombre] = Vertice(nombre, dato)
         return True
-    
-    def eliminar_vertice(self, nombre):
-        """Pre:el grafo esta creado.
-        Elimina el vertice y todas las aristas del mismo.
-        Pos: el grafo tiene un vertice menos y se eliminan las aristas del vertice."""
-        vertice = self.vertices.pop(nombre)
-        if vertice:
-            if self.es_dirigido:
-                for adyacente in self.vertices:
-                    if adyacente.estan_conectados(nombre):
-                        adyacente.eliminar_union(nombre)
-                        self.cantidad_aristas -= 1
-            else:
-                for adyacente in vertice.iterador_vertice():
-                    adyacente.eliminar_union(nombre)
-                    self.cantidad_aristas -= 1
-            self.cantidad_aristas -= vertice.cantidad_uniones()
 
-    def agregar_arista(self, inicio, fin, dato=None):
+    def agregar_arista(self, inicio, fin, peso=None):
         """Pre: el grafo fue creado.
         Retorna true si se creo la arista y false en caso contrario.
         Pos: se agrega una arista al grafo."""
@@ -42,29 +25,48 @@ class Grafo(object):
         vertice2 = self.vertices.get(fin)
         if not self.vertices_estan_unidos(inicio, fin):
             if self.es_dirigido:
-                vertice1.agregar_union(fin, dato)
+                vertice1.agregar_union(fin, peso)
                 self.cantidad_aristas += 1
                 return True
-            vertice1.agregar_union(fin, dato)
-            vertice2.agregar_union(inicio, dato)
+            vertice1.agregar_union(fin, peso)
+            vertice2.agregar_union(inicio, peso)
             self.cantidad_aristas += 1
             return True
         return False
 
-    def eliminar_arista(self, nombre1, nombre2):
+    def eliminar_arista(self, inicio, fin):
         """Pre: el grafo fue creado.
         Retorna true si se elimino la arista y false en caso que la arista no exista.
         Pos: se elimina una arista del grafo."""
-        vertice1 = self.get(nombre1)
-        vertice2 = self.get(nombre2)
-        if self.vertices_estan_unidos(nombre1, nombre2):
+        vertice1 = self.vertices.get(inicio)
+        vertice2 = self.vertices.get(fin)
+        if self.vertices_estan_unidos(inicio, fin):
             if self.es_dirigido:
-                vertice1.eliminar_union(nombre2) or vertice2.eliminar_union(nombre1)
+                vertice1.eliminar_union(fin)
+                self.cantidad_aristas -= 1
                 return True
-            vertice1.eliminar_union(nombre2)
-            vertice2.eliminar_union(nombre1)
+            vertice1.eliminar_union(fin)
+            vertice2.eliminar_union(inicio)
+            self.cantidad_aristas -= 1
             return True
         return False
+    
+    def eliminar_vertice(self, nombre):
+        """Pre:el grafo esta creado.
+        Elimina el vertice y todas las aristas del mismo.
+        Pos: el grafo tiene un vertice menos y se eliminan las aristas del vertice."""
+        if nombre in self.vertices:
+            if self.es_dirigido:
+                for adyacente in self.vertices:
+                    self.eliminar_arista(adyacente, nombre)
+                    self.eliminar_arista(nombre, adyacente)
+            else:
+                vertice = self.vertices.get(nombre)
+                for adyacente in vertice.iterador_vertice():
+                    adyacente = self.vertices.get(adyacente)
+                    adyacente.eliminar_union(nombre)
+                    self.cantidad_aristas -= 1
+            self.vertices.pop(nombre)
 
     def pertenece_vertice(self, nombre):
         """Indica si el vertice se encuentra en el grafo.
@@ -89,17 +91,6 @@ class Grafo(object):
             return self.vertices.get(inicio).estan_conectados(fin) and self.vertices.get(fin).estan_conectados(inicio)
         return False
 
-    def obtener_todos_los_vertices(self):
-        """Pre: el grafo existe.
-        Retorna una lista con todos los vertices."""
-        return self.vertices.keys()
-
-    def obtener_adyacentes(self, nombre):
-        """Pre: el grafo existe
-        Retorna los adyacentes a un vertice."""
-        vertice = self.vertices.get(nombre)
-        return vertice.ver_vecinos()
-
     def ver_peso_arista(self, inicio, fin):
         """Pre: el grafo existe.
         Se retorna el peso en el caso que la arista exista, None en el caso contrario."""
@@ -109,7 +100,39 @@ class Grafo(object):
             return vertice1.ver_peso_union(fin)    
         return None
 
+    def obtener_todos_los_vertices(self):
+        """Pre: el grafo existe.
+        Retorna una lista con todos los vertices."""
+        return self.vertices.keys()
+
+    def obtener_todas_las_aristas(self):
+        """Pre: el grafo existe.
+        Retorna una lista de listas con todas las aristas (inicio, fin y peso).
+        Retorna None en el caso que no haya aristas en el grafo."""
+        if self.cantidad_aristas == 0: return None
+        aristas = []
+        for nombre in self.vertices:
+            vertice = self.vertices.get(nombre)
+            for adyacente in vertice.iterador_vertice():
+                arista = []
+                arista[0] = nombre
+                arista[1] = adyacente
+                arista[2] = vertice.ver_peso_union(adyacente)
+                aristas.append(arista)
+        return aristas
+
+    def obtener_adyacentes(self, nombre):
+        """Pre: el grafo existe
+        Retorna los adyacentes a un vertice."""
+        vertice = self.vertices.get(nombre)
+        return vertice.ver_vecinos()
+
     def cantidad_vertices(self):
         """Pre: el grafo existe.
         Retorna la cantidad de vertices que hay en el grafo."""
         return len(self.vertices)
+    
+    def cantidad_uniones(self):
+        """Pre: el grafo existe.
+        Retorna la cantidad de aristas que hay en el grafo."""
+        return self.cantidad_aristas
