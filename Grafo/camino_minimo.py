202302c359c4eedc6008a6bbba7bff14f13abba2
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-10 00:37:42 -0300

    Se agrega el algoritmo de bellman-ford

--- Grafo/camino_minimo.py
+++ Grafo/camino_minimo.py
@@ -1,8 +1,10 @@
 from grafo import Grafo
 import queue
 
-#grafo pesado
+#grafo pesado con pesos positivos: usar dijkstra
+#grafo pesado, dirigido, con pesos negarivos: usar bellman-ford
 #grafo no pesado: usar un recorrido bfs
+#Orden O(e log(v)) siendo e la cantidad de aristas y v la cantidad de vertices.
 
 def dijkstra(grafo, origen):
     """"""
@@ -22,4 +24,24 @@ def dijkstra(grafo, origen):
                 distancia[adyacente] = distancia[vertice] + grafo.ver_peso_arista(vertice, adyacente)
                 padre[adyacente] = vertice
                 heap.put(adyacente, distancia[adyacente])
+    return padre, distancia
+
+def bellman_ford(grafo, origen):
+    """"""
+    distancia = {}
+    padre = {}
+    INFINITO = 'INFINITO'
+    for vertice in grafo:
+        distancia[vertice] = INFINITO
+    distancia[origen] = 0
+    padre[origen] = None
+    aristas = grafo.obtener_todas_las_aristas()
+    for i in range(grafo.cantidad_vertices()):
+        for v, w, peso in aristas:
+            if distancia[v] == INFINITO: continue
+            if (distancia[v] != INFINITO and distancia[w] == INFINITO) or (distancia[w] != INFINITO and distancia[v] + peso < distancia[w]):
+                padre[w] = v
+                distancia[w] = distancia[v] + peso
+    for v, w, peso in aristas:
+        if distancia[v] + peso < distancia[w]: return None #ciclo negativo
     return padre, distancia
