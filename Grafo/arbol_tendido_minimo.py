1035f0975b622993912f83ac9f924d004e11eb49
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-13 12:36:41 -0300

    Se agrega definicion de la complejidad

--- Grafo/arbol_tendido_minimo.py
+++ Grafo/arbol_tendido_minimo.py
@@ -1,6 +1,8 @@
 from grafo import Grafo
 import queue
+from union_find import UnionFind
 
+#Complejidad algoritmo de Prim: O(e log(v)) siendo e la cantidad de aristas y v la cantidad de vertices.
 def mst_prim(grafo):
     """"""
     vertice = grafo.vertice_aleatorio()
@@ -19,4 +21,18 @@ def mst_prim(grafo):
         visitados.add(w)
         for u in grafo.obtener_adyacentes(w):
             if u not in visitados: heap.put((w, u), grafo.ver_peso_arista(w, u))
+    return arbol
+
+def mst_kruskal(grafo):
+    """"""
+    conjuntos = UnionFind(grafo.obtener_todos_los_vertices())
+    aristas = sort(grafo.obtener_todas_las_aristas())
+    arbol = Grafo(False)
+    for v in grafo.obtener_todos_los_vertices():
+        arbol.agregar_vertice(v, grafo.ver_dato_vertice(v))
+    for a in aristas:
+        (v, w, peso) = a
+        if conjuntos.find(v) == conjuntos.find(w): continue
+        arbol.agregar_arista(v, w, peso)
+        conjuntos.union(v, w)
     return arbol
