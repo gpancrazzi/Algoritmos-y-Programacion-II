2cd986c043156cfdad3eb295be255f10b5f58c90
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-13 15:34:17 -0300

    Se agrega una funcion de coloreo

--- Grafo/backtraking.py
+++ Grafo/backtraking.py
@@ -5,6 +5,7 @@ def dfs_ciclo_largo_n(grafo, v, origen, n, visitados, camino_actual):
     visitados.add(v)
     if len(camino_actual) == n:
         if origen in grafo.obtener_adyacentes(v): return camino_actual
+        visitados.remove(v)
         return None
     for w in grafo.obtener_adyacentes(v):
         if w in visitados: continue
@@ -16,3 +17,26 @@ def dfs_ciclo_largo_n(grafo, v, origen, n, visitados, camino_actual):
 def ciclo_largo_n(grafo, origen, n):
     """"""
     return dfs_ciclo_largo_n(grafo, origen, origen, n, set(), [origen])
+
+def es_compatible(grafo, v, colores):
+    """"""
+    for w in grafo.obtener_adyacentes(v):
+        if w in colores and colores[w] == colores[v]: return False
+    return True
+
+def coloreo(grafo, v, colores, color, k):
+    """"""
+    colores[v] = color
+    if len(colores) == grafo.cantidad_vertices():
+        if es_compatible(grafo, v, colores): return True
+        colores.remove(v)
+        return False
+    if not es_compatible(grafo, v, colores):
+        colores.remove(v)
+        return False
+    for w in grafo.obtener_adyacentes(v):
+        if w in colores: continue
+        for color_opcion in range(k):
+            if coloreo(grafo, w, colores, color_opcion, k): return True
+    colores.remove(v)
+    return False
