795394e4e0d1057211912e2e1fe24522ed106ee9
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-08-13 20:04:00 -0300

    Se agrega funcion para construir el grafo mediante el archivo tsv

--- tp3/netstats.py
+++ tp3/netstats.py
@@ -3,10 +3,18 @@ from grafo import Grafo
 import sys
 
 def construir_grafo(archivo):
-    """"""
+    """Recibe un archivo, con formato tsv, abierto para lectura.
+    Retorna un grafo con las conexiones especificadas linea a linea."""
+    grafo = Grafo(False)
     for linea in archivo:
         linea_limpia = linea.strip()
         titulos = linea_limpia.split('\t')
+        articulo = titulos.pop(0)
+        grafo.agregar_vertice(articulo)
+        for link in titulos:
+            grafo.agregar_vertice(link)
+            grafo.agregar_arista(articulo, link)
+    return grafo
 
 #abrir el archivo pasado por terminal.
 archivo = open(sys.argv[1], 'r')
