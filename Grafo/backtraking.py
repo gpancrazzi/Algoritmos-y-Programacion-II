a1386e495c4cc34827e92f242192916c6bfaf7e1
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-09-07 12:39:59 -0300

    Agrego función para encontrar camino hamiltoniano.

--- Grafo/backtraking.py
+++ Grafo/backtraking.py
@@ -40,3 +40,23 @@ def coloreo(grafo, v, colores, color, k):
             if coloreo(grafo, w, colores, color_opcion, k): return True
     colores.remove(v)
     return False
+
+def camino_hamiltoniano_dfs(grafo, v, visitados, camino):
+    """"""
+    visitados.add(v)
+    camino.append(v)
+    if len(visitados) == grafo.cantidad_vertices(): return True
+    for w in grafo.obtener_adyacentes(v):
+        if w not in visitados:
+            if camino_hamiltoniano_dfs(grafo, w, visitados, camino): return True
+    visitados.remove(v)
+    camino.pop()
+    return False
+
+def camino_hamiltoniano(grafo):
+    """"""
+    camino = []
+    visitados = set()
+    for v in grafo.obtener_todos_los_vertices():
+        if camino_hamiltoniano_dfs(grafo, v, visitados, camino): return camino
+    return None
