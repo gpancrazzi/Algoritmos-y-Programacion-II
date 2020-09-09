690948805ca2b7ea8ff655772ab16fba96b307b4
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-09-09 18:32:04 -0300

    Se agregan definiciones de primitivas.

--- Grafo/union_find.py
+++ Grafo/union_find.py
@@ -3,12 +3,14 @@ class UnionFind(Object):
         self.groups = list(range(n))
 
     def find(self, v):
+        """Verifica a que conjunto pertene el nodo."""
         if self.groups[v] == v: return v
         real_group = self.find(self.groups[v])
         self.groups[v] = real_group
         return real_group
 
     def union(self, u, v):
+        """Une un nodo a un conjunto."""
         new_group = self.find(u)
         other = self.find(v)
         self.groups[other] = new_group
