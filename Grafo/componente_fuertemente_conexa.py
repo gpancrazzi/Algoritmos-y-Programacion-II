7332ba5fe777b069486651dae43924fde29d0e97
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-09-09 17:21:18 -0300

    Se corrigen bugs y se aplica el algoritmo para todos los vertices.

--- Grafo/componente_fuertemente_conexa.py
+++ Grafo/componente_fuertemente_conexa.py
@@ -2,31 +2,45 @@ from grafo import Grafo
 from pila import Pila
 
 def minimo(orden1, orden2):
-    """"""
-    if orden1 <= orden2:
-        return orden1
+    if orden1 <= orden2: return orden1
     return orden2
 
-def componente_fuertemente_conexa(grafo, vertice, visitados, pila, apilados, orden, mas_bajo, componentes):
-    """"""
-    visitados.add(vertice)
-    pila.apilar(vertice)
-    apilados.add(vertice)
-    mas_bajo[vertice] = orden[vertice]
-    for adyacente in grafo.obtener_adyacentes(vertice):
-        if adyacente not in visitados:
-            orden[adyacente] = orden[vertice] + 1
-            componente_fuertemente_conexa(grafo, adyacente, visitados, pila, apilados, orden, mas_bajo, componentes)
-            mas_bajo[vertice] = minimo(mas_bajo[vertice], mas_bajo[adyacente])
-        else if adyacente in apilados:
-            mas_bajo[vertice] = minimo(mas_bajo[vertice], mas_bajo[adyacente])
-    if mas_bajo[vertice] == orden[vertice]:
+def armar_nueva_cfc(pila, apilados, nueva_cfc):
+    v = pila.desapilar()
+    apilados.remove(v)
+    nueva_cfc.append(v)
+    return v
+
+def algoritmo_tarjan(grafo, v, visitados, pila, apilados, orden, mas_bajo, componentes):
+    visitados.add(v)
+    pila.apilar(v)
+    apilados.add(v)
+    mas_bajo[v] = orden[v]
+    for w in grafo.obtener_adyacentes(v):
+        if w not in visitados:
+            orden[w] = orden[v] + 1
+            algoritmo_tarjan(grafo, w, visitados, pila, apilados, orden, mas_bajo, componentes)
+            mas_bajo[v] = minimo(mas_bajo[v], mas_bajo[w])
+        else if w in apilados:
+            mas_bajo[v] = minimo(mas_bajo[v], mas_bajo[w])
+    if mas_bajo[v] == orden[v]:
         nueva_cfc = []
-        componente = pila.desapilar()
-        apilados.remove(componente)
-        nueva_cfc.append(componente)
-        while componente != vertice:
-            componente = pila.desapilar()
-            apilados.remove(componente)
-            nueva_cfc.append(componente)
+        w = armar_nueva_cfc(pila, apilados, nueva_cfc)
+        while w != v:
+            w = armar_nueva_cfc(pila, apilados, nueva_cfc)
         componentes.append(nueva_cfc)
+
+def componente_fuertemente_conexa(grafo):
+    """Calcula las componentes fuertemente conexas de todo el grafo.
+    Pre: el grafo existe.
+    Retorna una lista de listas, donde cada lista representa una componente."""
+    visitados = set()
+    pila = Pila()
+    apilados = set()
+    mas_bajo = {}
+    orden = {}
+    cfc = []
+    for v in grafo.obtener_todos_los_vertices(): 
+        orden[v] = 0
+        if v not in visitados: algoritmo_tarjan(grafo, v, visitados, pila, apilados, orden, mas_bajo, cfc)
+    return cfc
