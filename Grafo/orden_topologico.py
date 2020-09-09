7b73b8f3b9f847f8bb06d3a1d9c0579ee904214f
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-09-09 16:41:22 -0300

    Se corrigen bugs.

--- Grafo/orden_topologico.py
+++ Grafo/orden_topologico.py
@@ -3,46 +3,46 @@ from pila import Pila
 import queue
 
 def obtener_grados_de_entrada(grafo, grados):
-    """"""
-    for vertice in grafo.vertices:
-        grados[vertice] = 0
-    for vertice in grafo.vertices:
-        for adyacente in grafo.obtener_adyacentes(vertice):
-            grados[adyacente] += 1
+    for v in grafo.obtener_todos_los_vertices():
+        grados[v] = 0
+    for v in grafo.obtener_todos_los_vertices():
+        for w in grafo.obtener_adyacentes(v):
+            grados[w] = grados[w] + 1
 
 def orden_topologico_bfs(grafo):
-    """"""
+    """Retorna una lista con un orden topologico posible.
+    En caso de hallar un ciclo, retorna None."""
     grados = {}
     obtener_grados_de_entrada(grafo, grados)
     cola = queue.Queue()
-    for vertice in grafo.vertices:
-        if grados[vertice] == 0:
-            cola.put(vertice)
+    for v in grafo.obtener_todos_los_vertices():
+        if grados[v] == 0:
+            cola.put(v)
     resultado = []
     while not cola.empty():
-        vertice = cola.get()
-        resultado.append(vertice)
-        for adyacente in grafo.obtener_adyacentes(vertice):
-            grados[adyacente] -= 1
-            if grados[vertice] == 0:
-                cola.put(adyacente)
+        v = cola.get()
+        resultado.append(v)
+        for w in grafo.obtener_adyacentes(v):
+            grados[w] -= 1
+            if grados[w] == 0:
+                cola.put(w)
     if len(resultado) == grafo.cantidad_vertices():
         return resultado
     return None
 
-def orden_topologico_recursivo(grafo, vertice, pila, visitados):
-    """"""
-    visitados.add(vertice)
-    for adyacente in grafo.obtener_adyacentes(vertice):
-        if adyacente not in visitados:
-            orden_topologico_recursivo(grafo, vertice, pila, visitados)
-    pila.apilar(vertice)
+def orden_topologico_recursivo(grafo, v, pila, visitados):
+    visitados.add(v)
+    for w in grafo.obtener_adyacentes(v):
+        if w not in visitados:
+            orden_topologico_recursivo(grafo, v, pila, visitados)
+    pila.apilar(v)
 
 def orden_topologico_dfs(grafo):
-    """"""
+    """Retorna una lista con un orden topologico posible.
+    En caso de hallar un ciclo, retorna None."""
     visitados = set()
     pila = Pila()
-    for vertice in grafo.vertices:
-        if vertice not in visitados:
-            orden_topologico_recursivo(grafo, vertice, pila, visitados)
+    for v in grafo.obtener_todos_los_vertices():
+        if v not in visitados:
+            orden_topologico_recursivo(grafo, v, pila, visitados)
     return pila
