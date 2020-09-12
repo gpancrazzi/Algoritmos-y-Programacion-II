32705c5484c582f72ad5632687e8be35c5ea3471
Gustavo Roberto Pancrazzi <gpancrazzi@fi.uba.ar>
2020-09-12 12:54:42 -0300

    Se corrigen bugs.

--- Grafo/arbol_tendido_minimo.py
+++ Grafo/arbol_tendido_minimo.py
@@ -1,38 +1,44 @@
 from grafo import Grafo
-import queue
+import heapq
 from union_find import UnionFind
+from operator import itemgetter
 
 #Complejidad algoritmo de Prim: O(e log(v)) siendo e la cantidad de aristas y v la cantidad de vertices.
 def mst_prim(grafo):
-    """"""
+    """Pre: el grafo es no dirigido.
+    Retorna un arbol de tendido minimo."""
     vertice = grafo.vertice_aleatorio()
     visitados = set()
     visitados.add(vertice)
-    heap = queue.PriorityQueue()
+    heap = []
     arbol = Grafo(False)
     for v in grafo.obtener_todos_los_vertices():
         arbol.agregar_vertice(v, grafo.ver_dato_vertice(v))
     for w in grafo.obtener_adyacentes(vertice):
-        heap.put((vertice, w), grafo.ver_peso_arista(vertice, w))
+        heapq.heappush(heap, (grafo.ver_peso_arista(vertice, w), vertice, w))
     while not heap.empty():
-        (v, w) = heap.get()
+        (p, v, w) = heapq.heappop(heap)
         if w in visitados: continue
-        arbol.agregar_arista(v, w, grafo.ver_peso_arista(v, w))
+        arbol.agregar_arista(v, w, p)
         visitados.add(w)
-        for u in grafo.obtener_adyacentes(w):
-            if u not in visitados: heap.put((w, u), grafo.ver_peso_arista(w, u))
+        for e in grafo.obtener_adyacentes(w):
+            if e not in visitados: heapq.heappush(heap, (grafo.ver_peso_arista(w, e), w, e))
     return arbol
 
+#Complejidad algoritmo de Kruskal: O(e log(v)) siendo e la cantidad de aristas y v la cantidad de vertices.
+#Hay posibilidad de mejorar la complejidad si se tiene información que permita ordenar las aristas mediante
+#un algoritmo de ordenamiento no comparativo.
 def mst_kruskal(grafo):
-    """"""
+    """Pre: el grafo es no dirigido.
+    Retorna un arbol de tendido minimo."""
     conjuntos = UnionFind(grafo.obtener_todos_los_vertices())
-    aristas = sort(grafo.obtener_todas_las_aristas())
+    #ordenar las aristas de forma ascendente por peso.
+    aristas = sorted(grafo.obtener_todas_las_aristas(), key=itemgetter(2))
     arbol = Grafo(False)
     for v in grafo.obtener_todos_los_vertices():
         arbol.agregar_vertice(v, grafo.ver_dato_vertice(v))
-    for a in aristas:
-        (v, w, peso) = a
+    for v, w, p in aristas:
         if conjuntos.find(v) == conjuntos.find(w): continue
-        arbol.agregar_arista(v, w, peso)
+        arbol.agregar_arista(v, w, p)
         conjuntos.union(v, w)
     return arbol
